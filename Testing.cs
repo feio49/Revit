d511e38d7dee44813b4d29a8d87234693ac5a562
feio49 <arqt49@gmail.com>
2015-12-18 11:46:54 +0000

    Update Testing.cs

--- Testing.cs
+++ Testing.cs
@@ -1,1 +1,35 @@
+using System;
+using System.ComponentModel.Composition;
 
+namespace Feio
+{
+  public class ItemViewModel
+  {
+    private readonly IItem item;
+    private readonly IContentService contentService;
+    private readonly IViewModelFactory viewModelFactory;
+    
+    private DelegateCommand copyCommand;
+    private DelegateCommand deleteCommand;
+    
+    public ItemViewModel(IItem item, DocumentViewModel dvm, IContentService contentService, IViewModelFactory viewModelFactory)
+    {
+      this.item = item;
+      
+      this.contentService = contentService;
+      
+      this.viewModelFactory = viewModelFactory;
+      
+      deleteCommand = new DelegateCommand(() => item.Document.Items.Remove(item));
+    }
+    
+    public ICommand DeleteCommand => deleteCommand;
+    
+    public void Show() => contentService.Show(factory.Create(item));
+    {
+      var viewModel = factory.Create(item);
+      
+      contentService.Show(viewModel);
+    }
+  }
+}
